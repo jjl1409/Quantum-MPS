651cf2f920a55cf62b3beac44e5d3d03ef7eb85d
jjl1409 <amsjlam@gmail.com>
2026-04-28 16:37:25 -0400

    initial draft of decomposition function

--- mps.py
+++ mps.py
@@ -60,9 +60,56 @@ def mps_decomposition(psi: np.ndarray, p: int, max_bond_dim : int = 0) -> MPS:
     # Take log base p of total_dim to get n
     n = int(round(np.log(total_dim) / np.log(p)))
     # Reshape into a rank-n tensor
-    c = psi.reshape([p] * N)
+    c = psi.reshape([p] * n)
     
     tensors = []
-    remaining = C.copy()
+    remaining = c.copy()
 
+    # Handle all sites except the last one
+    for site in range(n - 1):
+        # Current shape is (left_bond * p, remaining_dims...)
+        # We want matrix with shape (left bond * p) x (p^(N - site - 1))
+        left_size = remaining.shape[0] * remaining_shape[1]
+
+        if site == 0:
+            # First site has shape (p, p^(n-1))
+            mat = remaining.reshape(p, -1)
+            left_bond = 1
+        else:
+            # Remaining sites have shape (D_left, p, p^{N - site - 1})
+            left_bond = remaining_shape[0]
+            mat = remaining.reshape(left_bond * p, -1)
     
+        # Perform SVD to get u . s . vh
+        # u has shape (m, k), vh has shape (k, n), and s has shape (k) (singular values)
+        u, s, vh = np.linalg.svd(mat, full_matrices = False)
+        d_bond = int(sum(s))
+
+        # D_bond has a minimum value of 1 and is bounded by max_bond_dim
+        if max_bond_dim > 0:
+            d_bond = max(min(D_bond, max_bond_dim), 1)
+
+        # Truncate to d_bond
+        u = u[:, :d_bond]
+        s = s[:d_bond]
+        vh = vh[:d_bond, :]
+
+        # Reshape u into a rank-3 tensor (left_bond, physical, right_bond)
+        a = u.reshape(left_bond, p, d_bond)
+        tensors.append(a)
+
+        # Diagonals of s x vh gives us our remaining singular values
+        # This has shape (d_bond, p^(n-site-1)) which needs to become (d_bond, p, p^(n-site-2))
+        remaining = np.diag(s) @ vh
+        remaining_sites = n - site - 1
+        if remaining_sites > 1:
+            remaining = remaining.reshape(d_bond, p, -1)
+    
+    # Handle last site where we are left with a rank-3 tensor (d_left, p, 1)
+    tensor = remaining.reshape(remaining.shape[0], p, 1)
+    tensors.append(tensor)
+
+    return MPS(tensors)
+
+
+
