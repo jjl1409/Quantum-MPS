c8daf3ea9411120f88b54ec875fe4c20341635e4
jjl1409 <amsjlam@gmail.com>
2026-04-28 03:04:15 -0400

    starting mps decomposition

--- mps.py
+++ mps.py
@@ -6,7 +6,7 @@ import numpy as np
 
  class MPS:
     # Instantiate MPS with a list of rank-3 tensors (left_bond, physical, right_bond)
-    def __init__(self, tensors : list):
+    def __init__(self, tensors: list):
         self.tensors = tensors
     
     # Return the number of sites from the list of tensors
@@ -18,6 +18,7 @@ import numpy as np
         return self.tensors[0].shape[1]
     
     # Return the list of bond dimensions between sites
+    # This is done by collecting right bond dimensions for each site except the last
     def bond_dims(self) -> list:
         return [t.shape[2] for t in self.tensors[:-1]]
 
@@ -31,4 +32,37 @@ import numpy as np
 # bipartition at bond k
 
 class CanonicalMPS:
-    def __init__(self, tensors :)
+    # Instantiate canonical MPS from a list of gammas and lambdas
+    # Gammas are a list of n tensors (left_bond, physical, right_bond)
+    # Lambdas are a list of n - 1 lambda vectors (Schmidt coefficients)
+    def __init__(self, gammas: list, lambdas: list):
+        self.gammas = gammas
+        self.lambdas = lambdas
+
+    # Return the number of sites for the list of vectors
+    def n(self) -> int:
+        return len(self.gammas)
+    
+    # Returns the list of bond dimensions between sites
+    # Note that this is equivalent to the bond_dims calculation for MPS (just semantics)
+    def bond_dims(self) -> list:
+        return [l.shape[0] in self.lambdas]
+
+# Decompose an N-particle quantum state into MPS using successive SVDs, returning an MPS
+# Successively performs Schmidt decompositions between site 1 and the rest, then site 2 and so on...
+# psi is a state vector of length p^Nn (aka a rank-n tensor with each index of dimension p)
+# p is the physical dimension per site
+# max_bond_dim is the maximum bond dimension d, where signular values beyond rank d are discarded.
+# Defaults to 0, which results in exact decomposition
+def mps_decomposition(psi: np.ndarray, p: int, max_bond_dim : int = 0) -> MPS:
+    # Total dimensions should be p * n
+    total_dim = np.prod(psi.shape)
+    # Take log base p of total_dim to get n
+    n = int(round(np.log(total_dim) / np.log(p)))
+    # Reshape into a rank-n tensor
+    c = psi.reshape([p] * N)
+    
+    tensors = []
+    remaining = C.copy()
+
+    
